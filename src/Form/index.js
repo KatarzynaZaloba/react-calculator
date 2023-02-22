5679f1058267f2ec54c52176b8d41bda392245e2
KatarzynaZaloba <kzalobafrontend@gmail.com>
2023-02-22 18:26:43 +0100

    Form added

---
+++ src/Form/index.js
@@ -0,0 +1,63 @@
+import "./style.css";
+import { currencies } from "../currencies";
+import { useState } from "react";
+import Result from "../Result";
+
+
+const Form = () => {
+
+    const [amount, setAmount] = useState("");
+    const [currency, setCurrency] = useState(currencies[0].short);
+    const rate = currencies.find(({ short }) => short === currency).rate;
+
+    return (
+        
+        <form className="form">
+            <h1 class="form__header">Kalkulator walutowy</h1>
+            <fieldset className="form__fieldset">
+                
+                <p>
+                    <label className="form__label">
+                        <span className="form__labelText">Wpisz kwotę w PLN:</span>
+                        <input
+                            className="form__field"
+                            type="number"
+                            step="0.01"
+                            required min="0"
+                            placeholder="Wpisz kwotę"
+                            value={amount}
+                            onChange={({target }) => setAmount(target.value)}
+                        />
+                    </label>
+                </p>
+                <p>
+                    <label className="form__label">
+                        <span className="form__labelText">Wybierz walutę</span>
+                        <select
+                            className="form__field"
+                            value={currency}
+                            onChange={({ target }) => setCurrency(target.value)}
+                        >
+                            {currencies.map((currency) => (
+                                <option
+                                    value={currency.short}
+                                    key={currency.short}
+                                >
+                                    {currency.name}
+                                </option>
+                            ))} 
+                        </select>
+                    </label>
+                </p>
+                
+                <Result 
+                amount={amount}
+                currency={currency}
+                rate={rate}
+                />              
+            </fieldset>
+        </form>
+    );
+};
+
+export default Form;
