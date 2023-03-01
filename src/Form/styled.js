9fb547320d930756028320fd13ede5e7504fc370
KatarzynaZaloba <kzalobafrontend@gmail.com>
2023-03-01 11:34:23 +0100

    removed space, formatted some code

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 
 export const StyledForm = styled.form`
-    border-radius: 5px;
-    margin: 20px;
+    border-radius: 2px;
+    margin: 10px;
 `;
 
 export const StyledDiv = styled.div`
     flex-basis: 700px;
-    margin: 20px;
-    padding: 20px;
+    margin: 10px;
+    padding: 10px;
     background: white;
-    border-radius: 20px;
+    border-radius: 10px;
     box-shadow: 0 0 30px black;
 
 `;
@@ -34,7 +34,7 @@ export const StyledSpan = styled.span`
     display: inline-block;
     text-align: center;
     color: black;
-    font-size: 20px;
+    font-size: 22px;
 `;
 
 export const StyledInput = styled.input`
@@ -49,7 +49,7 @@ export const StyledInput = styled.input`
 `;
 
 export const StyledSelect = styled.select`
-flex-basis: 500px;
+    flex-basis: 500px;
     width: 100%;
     padding: 5px;
     border: 2px solid #383535;
@@ -58,3 +58,36 @@ flex-basis: 500px;
     text-align: center;
     margin: 0 auto;
 `;
+
+export const StyledButton = styled.button`
+    width: 100%;
+    border: none;
+    color: white;
+    background-color: hsl(180, 94%, 27%);
+    padding: 10px;
+    border-radius: 5px;
+    margin-bottom: 20px;
+    cursor: pointer;
+
+    &:hover {
+        filter: brightness(110%);
+        transform: scale(1.03);
+    }
+    &:active {
+        filter: brightness(120%);
+    }
+`;
+
+export const StyledParagraph = styled.p`
+    text-align: center;
+`;
+
+export const StyledParagraphLoading = styled.p`
+    color: teal;
+    text-align: center;
+;`
+
+export const StyledParagraphFailure = styled.p`
+    color: crimson;
+    text-align: center;
+;`
